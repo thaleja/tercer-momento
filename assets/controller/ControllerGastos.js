0d3d548312172b8ab5dc6ebfe66a81548a4d26aa
Juanmrno <jmrno14@hotmail.com>
2023-11-23 19:34:28 -0500

    se realiza la seccion listar

--- assets/controller/ControllerGastos.js
+++ assets/controller/ControllerGastos.js
@@ -3,107 +3,133 @@ console.log(gastos);
 
 // Objeto para gestionar la sesión
 let sesion = {
-  usuarioActual: null,
+   usuarioActual: null,
 };
 
 // Botón para iniciar sesión
-let btnIniciar = document.getElementById('btnIniciar');
+let btnIniciar = document.getElementById("btnIniciar");
 // Botón para cerrar sesión (renombrado a camelCase)
-let cerrarSesionBtn = document.getElementById('cerrarSesion');
+let cerrarSesionBtn = document.getElementById("cerrarSesion");
 
 // Evento para iniciar sesión
-btnIniciar.addEventListener('click', function () {
-    console.log('Iniciando sesión');
-    document.getElementById('spinner').style.display = "block";
-    setTimeout(() => {
-        document.getElementById('spinner').style.display = "none";
-        document.getElementById('login').style.display = "none";
-        document.getElementById('panel').style.display = "block";
-        sesion.usuarioActual = document.getElementById("usuario").value; // Obtener el usuario del formulario de inicio de sesión
-        actualizarNombreUsuario();
-    }, 3000);
+btnIniciar.addEventListener("click", function () {
+   console.log("Iniciando sesión");
+   document.getElementById("spinner").style.display = "block";
+   setTimeout(() => {
+      document.getElementById("spinner").style.display = "none";
+      document.getElementById("login").style.display = "none";
+      document.getElementById("panel").style.display = "block";
+      sesion.usuarioActual = document.getElementById("usuario").value; // Obtener el usuario del formulario de inicio de sesión
+      actualizarNombreUsuario();
+   }, 3000);
 });
 
 // Evento para cerrar sesión
-cerrarSesionBtn.addEventListener('click', function () {
-    cerrarSesion();
+cerrarSesionBtn.addEventListener("click", function () {
+   cerrarSesion();
 });
 
 // Evento cuando se carga el contenido del documento
-document.addEventListener('DOMContentLoaded', function () {
-    const btnRegistraR = document.getElementById('btnRegistraR');
-    btnRegistraR.addEventListener('click', validarRegistro);
+document.addEventListener("DOMContentLoaded", function () {
+   const btnRegistraR = document.getElementById("btnRegistraR");
+   btnRegistraR.addEventListener("click", validarRegistro);
 });
 
 // Función para validar el registro
 function validarRegistro() {
-    let mensajeExito = document.createElement('p');
-    mensajeExito.textContent = 'Formulario de registro validado con éxito';
-    mensajeExito.classList.add('mensaje-exito'); // Agrega una clase al elemento
-    document.body.appendChild(mensajeExito);
+   let mensajeExito = document.createElement("p");
+   mensajeExito.textContent = "Formulario de registro validado con éxito";
+   mensajeExito.classList.add("mensaje-exito"); // Agrega una clase al elemento
+   document.body.appendChild(mensajeExito);
 }
 
 // Función para cerrar sesión
 function cerrarSesion() {
-    // Elimina la información de la sesión (puedes implementar tu lógica específica)
-    sesion.usuarioActual = null;
+   // Elimina la información de la sesión (puedes implementar tu lógica específica)
+   sesion.usuarioActual = null;
 
-    // Actualiza el nombre del usuario en la sección del panel (puedes ajustarlo según tu lógica)
-    actualizarNombreUsuario();
+   // Actualiza el nombre del usuario en la sección del panel (puedes ajustarlo según tu lógica)
+   actualizarNombreUsuario();
 
-    // Oculta el panel actual y muestra el formulario de inicio de sesión
-    document.getElementById('panel').style.display = "none";
-    document.getElementById('login').classList.add("visible");
+   // Oculta el panel actual y muestra el formulario de inicio de sesión
+   document.getElementById("panel").style.display = "none";
+   document.getElementById("login").classList.add("visible");
 }
 
-let registrarGastos = document.getElementById('registrarGastos');
-registrarGastos.addEventListener('click', function () {
-    document.getElementById('sectionRegistrarGastos').style.zIndex = '3';
-    document.getElementById('sectionListarGastos').style.zIndex = '2';
-    document.getElementById('sectionMovimientosGastos').style.zIndex = '2';
+let registrarGastos = document.getElementById("registrarGastos");
+registrarGastos.addEventListener("click", function () {
+   document.getElementById("sectionRegistrarGastos").style.zIndex = "3";
+   document.getElementById("sectionListar").style.zIndex = "2";
+   document.getElementById("sectionMovimientosGastos").style.zIndex = "2";
 });
 
 let btnGuardar = document.getElementById("btnGuardar");
 btnGuardar.addEventListener("click", () => {
-    let id = Math.random() * 100;
-    let categoria = document.getElementById("categoria").value;
-    let descripcion = document.getElementById("descripcion").value;
-    let valor = document.getElementById("valor").value;
+   let gcategoria = (document.getElementById("gcategoria").value = "");
+   let gdescripcion = (document.getElementById("gdescripcion").value = "");
+   let gvalor = (document.getElementById("gvalor").value = "");
+   let gfecha = (document.getElementById("gfecha").value = "");
 
-    let gasto = {
-        id, categoria, descripcion, valor
-    };
+   let gasto = {
+      id: Math.random() * 100,
+      categoria: gcategoria,
+      descripcion: gdescripcion,
+      valor: gvalor,
+      fecha: gfecha,
+   };
 
-    gastos.push(gasto);
-    movimientos();
+   gastos.push(gasto);
+
+   document.getElementById("gcategoria").value = "";
+   document.getElementById("gdescripcion").value = "";
+   document.getElementById("gvalor").value = "";
+   document.getElementById("gfecha").value = "";
+
+   movimientos();
 });
 
-let listarGastos = document.getElementById('listarGastos');
+let listarGastos = document.getElementById("listarGastos");
 
-listarGastos.addEventListener('click', function () {
-    document.getElementById('sectionRegistrarGastos').style.zIndex = '2';
-    document.getElementById('sectionListarGastos').style.zIndex = '3';
-    document.getElementById('sectionMovimientosGastos').style.zIndex = '2';
-    
-    gastos.map((gasto) => {
-        console.log(gasto);
-    });
+listarGastos.addEventListener("click", function () {
+   document.getElementById("sectionRegistrarGastos").style.zIndex = "2";
+   document.getElementById("sectionListar").style.zIndex = "3";
+   document.getElementById("sectionMovimientosGastos").style.zIndex = "2";
+
+   gastos.map((gasto) => {
+      let categoria = document.createElement("p");
+      categoria.textContent = gasto.categoria;
+
+      let descripcion = document.createElement("p");
+      descripcion.textContent = gasto.descripcion;
+
+      let valor = document.createElement("p");
+      valor.textContent = gasto.valor;
+
+      let fecha = document.createElement("p");
+      fecha.textContent = gasto.fecha;
+
+      let card = document.createElement("article");
+      card.classList.add("card");
+      card.append(categoria, descripcion, valor, fecha);
+
+      document.getElementById("sectionListar").append(card);
+   });
 });
 
-let movimientosGastos = document.getElementById('movimientosGastos');
-movimientosGastos.addEventListener('click', function () {
-    document.getElementById('sectionRegistrarGastos').style.zIndex = '2';
-    document.getElementById('sectionListarGastos').style.zIndex = '2';
-    document.getElementById('sectionMovimientosGastos').style.zIndex = '3';
+let movimientosGastos = document.getElementById("movimientosGastos");
+movimientosGastos.addEventListener("click", function () {
+   document.getElementById("sectionRegistrarGastos").style.zIndex = "2";
+   document.getElementById("sectionListar").style.zIndex = "2";
+   document.getElementById("sectionMovimientosGastos").style.zIndex = "3";
 });
 
 function movimientos() {
-    console.log("Guardando movimiento");
+   console.log("Guardando movimiento");
 }
 
 function actualizarNombreUsuario() {
-    let nombreUsuarioSpan = document.getElementById("nombreUsuarioSpan");
-    if (nombreUsuarioSpan) {
-        nombreUsuarioSpan.textContent = sesion.usuarioActual;
-    }
+   let nombreUsuarioSpan = document.getElementById("nombreUsuarioSpan");
+   if (nombreUsuarioSpan) {
+      nombreUsuarioSpan.textContent = sesion.usuarioActual;
+   }
 }
--- assets/model/ModelGastos.js
+++ assets/model/ModelGastos.js
@@ -1,12 +1,53 @@
 export let gastos = [
-    {  id: 10,
-        categoria: 'comidas',
-        descripcion: 'deliciosa comida chatarra',
-        valor: 15000,   
-    },
-]
+   {
+      id: 1,
+      categoria: "comidas",
+      descripcion: "deliciosa comida chatarra",
+      valor: 15000,
+      fecha: 11 / 2 / 2023,
+   },
+   {
+      id: 2,
+      categoria: "Negocios",
+      descripcion: "beaticos",
+      valor: 100.0,
+      fecha: 20 / 4 / 2023,
+   },
+   {
+      id: 3,
+      categoria: "Diversion",
+      descripcion: "Fiesta de cumpleaños de Maria",
+      valor: 60000,
+      fecha: 5 / 6 / 2023,
+   },
+   {
+      id: 4,
+      categoria: "Transporte",
+      descripcion: "pago del envio del computador",
+      valor: 20000,
+      fecha: 2 / 9 / 2023,
+   },
+   {
+      id: 5,
+      categoria: "Negocio",
+      descripcion: "fotocopias",
+      valor: 8000,
+      fecha: 14 / 2 / 2023,
+   },
+   {
+      id: 6,
+      categoria: "comidas",
+      descripcion: "pizza para tiempo extra de noche",
+      valor: 45000,
+      fecha: 9 / 10 / 2023,
+   },
+   {
+      id: 7,
+      categoria: "Diversion",
+      descripcion: "Fiesta de cumpleaños de Cristian",
+      valor: 60000,
+      fecha: 24 / 5 / 2023,
+   },
+];
 
-export let usuarioInicioSesion = [
-    {  usuario :'cristian'
-    },
-]
+export let usuarioInicioSesion = [{ usuario: "cristian" }];
